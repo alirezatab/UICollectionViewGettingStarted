6c3dceff0e4b80d011fad49b1ef2316045c3826a
alirezatab <alirezatabrizi10@gmail.com>
2016-12-04 01:53:19 -0800

    get a hear to show up on top of collection View

---
+++ UICollectionViewGettingStarted/FlickerPhotoHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  FlickerPhotoHeaderView.swift
+//  UICollectionViewGettingStarted
+//
+//  Created by ALIREZA TABRIZI on 12/4/16.
+//  Copyright © 2016 AR-T.com, Inc. All rights reserved.
+//
+
+import UIKit
+
+class FlickerPhotoHeaderView: UICollectionReusableView {
+    @IBOutlet weak var label : UILabel!
+}
--- UICollectionViewGettingStarted/FlickrPhotoHeaderView.swift
+++ UICollectionViewGettingStarted/FlickrPhotoHeaderView.swift
@@ -68,6 +68,23 @@ extension FlickerPhotosViewController {
         return searches[section].searchResults.count
     }
     
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        // 1 - The kind parameter is supplied by the layout object and indicates which sort of supplementary view is being asked for
+        switch kind {
+        // 2 - UICollectionElementKindSectionHeader is a supplementary view kind belonging to the flow layout. By checking that box in the storyboard to add a section header, you told the flow layout that it needs to start asking for these views. There is also a UICollectionElementKindSectionFooter, which you’re not currently using. If you don’t use the flow layout, you don’t get header and footer views for free like this
+        case UICollectionElementKindSectionHeader:
+            // 3 - The header view is dequeued using the identifier added in the storyboard. This works just like cell dequeuing. The label’s text is then set to the relevant search term
+            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "FlickerPhotoHeaderView", for: indexPath) as! FlickerPhotoHeaderView
+            headerView.label.text = searches[(indexPath as NSIndexPath).section].searchTerm
+            return headerView
+            
+        default:
+            // 4 - An assert is placed here to make it clear to other developers (including future you!) that you’re not expecting to be asked for anything other than a header view
+            assert(false, "Unexpected elemnd kind")
+        }
+    }
+
+    
     //3
     override func collectionView(_ collectionView: UICollectionView,
                                  cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -109,3 +126,19 @@ extension FlickerPhotosViewController : UICollectionViewDelegateFlowLayout {
         return sectionInsets.left
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
